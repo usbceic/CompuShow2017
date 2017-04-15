cc3a36cc1eb7a37f78a5b3ccc5b34593c4baaab7
Jose Acevedo <joseluisacevedo1995@gmail.com>
2017-04-14 20:26:09 -0400

    Add script to load data into compusoft database

---
+++ loaddb.sql
@@ -0,0 +1,37 @@
+----------------------------------------------------------
+--														--
+--       CompuSoft - The Compushow 2017 Software	    --
+--														--
+----------------------------------------------------------
+--														--
+--		Script to load data to CompuSoft database.		--
+--														--
+----------------------------------------------------------
+
+-- Load Compushow categories
+INSERT INTO category (name, description) 
+VALUES
+	('CompuCono','Pensándolo bien, el cono es hasta más útil que tú.'),
+	('CompuPapi','El más guapo de Computación, ese que está bueno y con el que todas quieren.'),
+	('CompuMami','La más bella de Computación, esa que entra a la sala y todos la desean.'),
+	('CompuGordito','Mantiene deudas altas en el cuartico y no se pela una dona.'),
+	('CompuAdoptado','No es de la carrera pero se la pasa en la sala y sabe más de nuestras materias que nosotros mismos.'),
+	('CompuMaster','El/La profe más respetad@ de Computación.'),
+	('CompuFalso','Iban a ser pareja en el laboratorio y bueeh…'),
+	('CompuTeam','El equipo más activo de Computación.'),
+	('CompuCuchi','Awwwwwwww…'),
+	('CompuChancero','Ese que no puede ver a una chama porque le empieza a caer con la “mejor” de sus labias.'),
+	('CompuPro','Va pensum, no ha retirado nada y saca 5 en todas las materias... Quisieras ser como él.'),
+	('CompuFitness','Tiene tanta fuerza que puede levantar los servidores del LDC.'),
+	('CompuLove','La parejita más linda de computación.'),
+	('CompuChapita','Se reúnen para tomar unas birras y a la primera ya no puede con su vida.'),
+	('CompuTukky','Cuando lo veas, mejor guarda el celular.'),
+	('CompuLolas','¿Serán operadas?'),
+	('CompuComadre','La más sociable de Computación.'),
+	('CompuCompadre','El más sociable de Computación.'),
+	('CompuCartoon','Ese amigo que parece que lo sacaron de la televisión.'),
+	('CompuProductista','¿Qué es Linux? ¿Qué lenguaje es ese?'),
+	('CompuButt','¿Será operado?'),
+	('CompuBully','Esa persona que se la pasa burlándose de todos, basta que le digas "¡Hola!" para que haga un chiste con eso.'),
+	('CompuCuaima','¿Quien es esa %$!* que te está escribiendo “hola hijo como estas ya comiste”? Seguro es la otra!'),
+	('CompuSaurio','Ese amigo que ya le comienzas a ver las canas..');
