6940b1061090a6cefb662cc0935426e29023ea9c
Jose Acevedo <joseluisacevedo1995@gmail.com>
2017-11-16 12:04:42 +0100

    add settings files

--- compusoft/settings.py
+++ compusoft/settings.py
@@ -120,6 +120,6 @@ LOGIN_URL = '/login/'
 # Accont activation email
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'usbceic@gmail.com'
-EMAIL_HOST_PASSWORD = 'insert_ceic_password'
+EMAIL_HOST_USER = 'compushowusb@gmail.com'
+EMAIL_HOST_PASSWORD = 'pizzabrownie11'
 EMAIL_PORT = 587
